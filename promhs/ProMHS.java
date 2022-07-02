a55c27e5369b3f3cb5d84e57abe960add3e5b03e
Anjelioktalia20103298 <101331758+Anjelioktalia20103298@users.noreply.github.com>
2022-07-02 20:48:07 +0800

    Add files via upload

---
+++ promhs/ProMHS.java
@@ -0,0 +1,14 @@
+package promhs;
+
+/**
+ *
+ * @author Anjeli Oktalia_20103298
+ */
+
+public class ProMHS {
+
+    public static void main(String[] args) {
+        new frmSIM().setVisible(true);
+    }
+    
+}
---
+++ promhs/loadIMG.java
@@ -0,0 +1,21 @@
+package promhs;
+
+/**
+ *
+ * @author Anjeli Oktalia_20103298
+ */
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+
+public class loadIMG {
+    
+    public static BufferedImage loadImage(String ref){
+        BufferedImage bimg = null;
+        try{
+            bimg = ImageIO.read(new File(ref));
+        }catch(Exception e){}
+        return bimg;
+    }
+}
